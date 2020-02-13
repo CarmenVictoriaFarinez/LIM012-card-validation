820db597eed3d992b94cf3022857c0c06c2929e1
CarmenVictoriaFarinez <acuario1140@gmail.com>
2020-02-12 22:03:03 -0500

    Suma los pares mayores a 10 en el algoritmo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-import validator from './validator.js';
+//import validator from './validator.js';
+let btn = document.getElementById('btn');
+btn.addEventListener ('click', validar);
 
-console.log(validator);
+function validar () {
+  let numberCard = document.getElementById('card').value;
+  array = Array.from(numberCard);
+  let newArray = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    newArray = newArray + array[i];
+    
+    let sumador = 0;
+    for (i = 0; i <= newArray.length; i++) {
+      if (i % 2 !== 0) {
+        let positionPar = parseInt(newArray[i]) * 2;
+        
+        if (parseInt(positionPar) >= 10) {
+         let valor = positionPar.toString();
+         let valor1 = Array.from (valor);
+
+         
+         let suma=0;
+         for (x = 0; x < valor.length; x++){
+         suma = suma + parseInt (valor1[x]); 
+         }
+         sumador = sumador + suma;
+         alert (suma);
+        }
+      }
+    }
+  }
+}
+
+//console.log(validator);
