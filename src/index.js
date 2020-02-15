1905672b0f3678634313539a1269660110966aae
CarmenVictoriaFarinez <acuario1140@gmail.com>
2020-02-15 11:50:49 -0500

    validado

--- src/index.js
+++ src/index.js
@@ -1,34 +1,10 @@
-//import validator from './validator.js';
-let btn = document.getElementById('btn');
+import validator from './validator.js';
+
+  let btn = document.getElementById('btn');
+  let validar = () => {
+  let tarjeta = document.getElementById('card').value;
+  let numero_tarjeta = validator.isValid (tarjeta);
+
+   }
 btn.addEventListener ('click', validar);
-
-function validar () {
-  let numberCard = document.getElementById('card').value;
-  array = Array.from(numberCard);
-  let newArray = [];
-  for (let i = array.length - 1; i >= 0; i--) {
-    newArray = newArray + array[i];
-    
-    let sumador = 0;
-    for (i = 0; i <= newArray.length; i++) {
-      if (i % 2 !== 0) {
-        let positionPar = parseInt(newArray[i]) * 2;
-        
-        if (parseInt(positionPar) >= 10) {
-         let valor = positionPar.toString();
-         let valor1 = Array.from (valor);
-
-         
-         let suma=0;
-         for (x = 0; x < valor.length; x++){
-         suma = suma + parseInt (valor1[x]); 
-         }
-         sumador = sumador + suma;
-         alert (suma);
-        }
-      }
-    }
-  }
-}
-
-//console.log(validator);
+console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,38 @@
 const validator = {
-  // ...
+  isValid(numero_tarjeta){
+//Prueba para numero_tarjeta =123456//
+   let cadena = numero_tarjeta.toString();
+   let longitud = cadena.length;
+   let cifraPares = 0;
+   let cifra_cad=0;
+   let suma=0;
+ for (i=0; i < longitud; i++){
+   if (i % 2 !== 0) {
+   cifraPares = parseInt(cadena.charAt(i))*2;
+   //console.log (cifraPares); 4,8,12
+   if (cifraPares > 9){ 
+     cifra_cad = cifraPares.toString();
+     cifraPares = parseInt(cifra_cad.charAt(0)) + 
+parseInt(cifra_cad.charAt(1));
+    //console.log (cifraPares); 1+2 =3
+   }
+   suma=suma + cifraPares;
+   //console.log (suma); 4, 12, 15
+   }
+ }
+ for ( i=1; i < longitud; i+=2){
+   suma += parseInt(cadena.charAt(i));
+   //console.log (suma);
+ }
+	
+ if ((suma % 10) == 0){ 
+  alert("Número de tarjeta correcto");
+  console.log (suma);
+ } else {
+  alert("El número de tarjeta no es válido");
+  console.log (suma);
+ }
+}
 };
 
 export default validator;
