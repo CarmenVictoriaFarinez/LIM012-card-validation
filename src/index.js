c5d5f6a242c1992cbaebccc53eef23cb98907bb2
CarmenVictoriaFarinez <acuario1140@gmail.com>
2020-02-22 23:55:51 -0500

    modificado2202

--- src/index.js
+++ src/index.js
@@ -10,19 +10,25 @@ if (validator.isValid (tarjeta) == true){
   document.getElementById('ver').innerHTML = "Your card is valid";
 }else {
   document.getElementById('ver').innerHTML = "Your card is invalid";
-
-//document.getElementById ('ver').style.display(none);
-//document.getElementById ('ver').style.display('');
+ 
 }
 }
   btn.addEventListener ('click', validar);
 
-   const ocultar = () => {
-   const tarjeta = document.getElementById('card').value;
-   document.getElementById('ver1').innerHTML = validator.maskify (tarjeta);
+const ocultar = () => {
+const tarjeta = document.getElementById('card').value;
+document.getElementById('ver1').innerHTML = validator.maskify (tarjeta);
     
-   };
-   btn.addEventListener ('click', ocultar);
+}
+btn.addEventListener ('click', ocultar);
+
+const ganar = document.getElementById('ganar');
+const gift = () => {
+const inst = document.getElementById('ganar').value;
+
+document.getElementById('ver2').classList.remove ('borrar');
+};
+ganar.addEventListener ('click', gift);
 
 //console.log(validator);
 
