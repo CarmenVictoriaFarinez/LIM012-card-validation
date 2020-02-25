254f829e22d8e6dc82355c670116b6ebd42cb4e2
CarmenVictoriaFarinez <acuario1140@gmail.com>
2020-02-24 23:59:15 -0500

    modificado2402

--- src/index.js
+++ src/index.js
@@ -1,34 +1,48 @@
 import validator from './validator.js';
+
+
+
 //Extrae mi input //
 const btn = document.getElementById('btn');
-
 const validar = () => {
 const tarjeta = document.getElementById('card').value;
 
-
-if (validator.isValid (tarjeta) == true){
+//if (tarjeta == ''){
+  //document.getElementById('alerta').innerHTML = "Por favor escriba un numero de tarjeta";
+  //document.getElementById('alerta').classList.add ();
+ if  (validator.isValid (tarjeta) == true){
   document.getElementById('ver').innerHTML = "Your card is valid";
 }else {
   document.getElementById('ver').innerHTML = "Your card is invalid";
  
+  }
 }
-}
-  btn.addEventListener ('click', validar);
+//}
 
-const ocultar = () => {
+  btn.addEventListener ('click', validar);
+  
+
+const enmascarar = () => {
 const tarjeta = document.getElementById('card').value;
 document.getElementById('ver1').innerHTML = validator.maskify (tarjeta);
     
 }
-btn.addEventListener ('click', ocultar);
+btn.addEventListener ('click', enmascarar);
 
-const ganar = document.getElementById('ganar');
+const pasos = document.getElementById('pasos');
 const gift = () => {
-document.getElementById('ganar').value;
-
-document.getElementById('ver2').classList.remove ('borrar');
+document.getElementById('pasosver2').classList.remove ('borrar');
+document.getElementById('pasosver3').classList.add('borrar');
 };
-ganar.addEventListener ('click', gift);
+
+pasos.addEventListener ('click', gift);
+
+const principal = document.getElementById('principal');
+const pri = () => {
+   document.getElementById('pasosver3').classList.remove('borrar');
+   document.getElementById('pasosver2').classList.add('borrar');
+ };
+principal.addEventListener ('click', pri);
 
 //console.log(validator);
 
